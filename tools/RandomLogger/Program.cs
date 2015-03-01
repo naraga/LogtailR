4fa5a8b06a30346875f7927ae102632f9e0e0451
Boris Bucha <naraga@gmail.com>
2015-03-01 22:26:18 +0100

    RandomLogger variable delay and more randomized log level

--- tools/RandomLogger/Program.cs
+++ tools/RandomLogger/Program.cs
@@ -10,12 +10,11 @@ namespace RandomLogger
         {
             const string logDir = @"c:\temp\logtailrlogs";
             const int logFilesCount = 3;
-            const int writeIntervalMs = 500;
 
             var loggers = new List<RandomLogger>();
             for (int i = 0; i < logFilesCount; i++)
             {
-                var logger = new RandomLogger(logDir, writeIntervalMs: writeIntervalMs);
+                var logger = new RandomLogger(logDir, string.Format("rndlog{0:00}", i));
                 loggers.Add(logger);
                 Console.WriteLine("Will write to {0}", logger.LogFileName);
                 Task.Factory.StartNew(logger.StartWriting, TaskCreationOptions.LongRunning);
--- tools/RandomLogger/RandomLogger.cs
+++ tools/RandomLogger/RandomLogger.cs
@@ -6,15 +6,19 @@ namespace RandomLogger
 {
     class RandomLogger
     {
-        private readonly int _writeIntervalMs;
+        private readonly string _loggerName;
+        private readonly int _writeIntervalFromMs;
+        private readonly int _writeIntervalToMs;
         private readonly string _logFileName;
         private readonly Random _rnd;
 
-        public RandomLogger(string directory, string logFileName = null, int writeIntervalMs = 1000)
+        public RandomLogger(string directory, string loggerName, string logFileName = null, int writeIntervalFromMs = 500, int writeIntervalToMs = 1000)
         {
-            _writeIntervalMs = writeIntervalMs;
+            _loggerName = loggerName;
+            _writeIntervalFromMs = writeIntervalFromMs;
+            _writeIntervalToMs = writeIntervalToMs;
             _logFileName = Path.Combine(directory, logFileName ?? string.Format("{0:yyyyMMdd_HHmmss}_{1}.log", DateTime.Now, DateTime.Now.Ticks));
-            _rnd = new Random();
+            _rnd = new Random((int)DateTime.Now.Ticks);
         }
 
         public string LogFileName
@@ -27,7 +31,7 @@ namespace RandomLogger
             while (true)
             {
                 File.AppendAllLines(_logFileName, new []{GetNewLogRecord()});
-                Thread.Sleep(_writeIntervalMs);
+                Thread.Sleep(_rnd.Next(_writeIntervalFromMs, _writeIntervalToMs));
             }
 // ReSharper disable once FunctionNeverReturns
         }
@@ -40,9 +44,10 @@ namespace RandomLogger
 
         string GetNewLogRecord(LogLevel level)
         {
-            return string.Format("{0:yyyy-MM-dd HH:mm:ss.fff}:{1}:{2:n}", 
+            return string.Format("{0:yyyy-MM-dd HH:mm:ss.fff}:{1}:{2}:the message{3:n}\nmsggggggg{3:n}xxxx", 
                 DateTime.Now,
                 level,
+                _loggerName,
                 Guid.NewGuid()
                 );
         }
